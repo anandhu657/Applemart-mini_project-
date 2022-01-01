25fcda1cea6d4933407dbcaf98a1551a3ebffc19
Anandhu M S <anandhumeleveettil@gmail.com>
2022-01-01 22:45:30 +0530

    signup page

---
+++ signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="./css/signin.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="signup-container">
+            <form action="#" class="signup-form" method="POST">
+                <div class="user-name">
+                    <input type="text" name="fname" placeholder="First name" max="15">
+                    <input type="text" name="lname" placeholder="Last name">
+                </div>
+                <div class="user-username">
+                    <input type="email" name="email" 
+                    placeholder="Enter your username (gmail,outlook,yahoo)">
+                </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
