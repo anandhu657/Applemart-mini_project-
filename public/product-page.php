2ef6e548f1cc8455b94193a2dda135271a0d1c30
Anandhu M S <anandhumeleveettil@gmail.com>
2022-02-10 19:56:44 +0530

    Add product page

---
+++ public/product-page.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iphone</title>
+    <link rel="stylesheet" href="css/product.css">
+</head>
+<body>
+    <div class="container">
+        <div class="image-sec">
+            <img src="images/iphone_12_mini.jpeg" alt="">
+        </div>
+        <div class="details">
+            <div class="product-name">
+                <h1>Apple Iphone 13 PRO MAX</h1>
+                <a href="https://www.apple.com/in/">Visit the site</a>
+            </div>
+            <hr>
+            <div class="product-price">
+                <div class="price-details">
+                    <h3>MRP: ₹4000</h3>
+                    <p><i>Inclusive of all taxes</i></p>
+                    <p><strong>EMI</strong> starts at ₹1,789. No Cost EMI available EMI</p>
+                    <p>7-days replacement only.</p>
+                    <strong>FREE DELIVERY</strong>
+                </div>
+                <div class="price-button">
+                    <button>BUY</button>
+                </div>
+            </div>
+            <hr>
+            <div class="product-details">
+                <table>
+                    <tr>
+                        <th>Model</th>
+                        <td>Iphone 13  pro max</td>
+                    </tr>
+                    <tr>
+                        <th>color</th>
+                        <td>Red</td>
+                    </tr>
+                    <tr>
+                        <th>Display</th>
+                        <td>6.1-inch XDR display</td>
+                    </tr>
+                    <tr>
+                        <th>Chip</th>
+                        <td>A12 Bionic chip</td>
+                    </tr>
+                    <tr>
+                        <th>Memory</th>
+                        <td>64GB</td>
+                    </tr>
+                    <tr>
+                        <th>Camera</th>
+                        <td>Dual 12MP camera</td>
+                    </tr>
+                    <tr>
+                        <th>Front camera</th>
+                        <td>12MP TrueDepth camera</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
